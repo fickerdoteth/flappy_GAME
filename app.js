a949d7374a3ed3ebc9b01b86567612de5f9120f9
fickerdoteth <126481093+fickerdoteth@users.noreply.github.com>
2023-10-22 22:20:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const bird = document.querySelector('.bird');
     const gameDisplay = document.querySelector('.game-container');
     const ground = document.querySelector('.ground-moving');
+    const retryButton = document.querySelector('.retry-button'); // Get the "Retry" button element
 
     let birdLeft = 220;
     let birdBottom = 100;
@@ -9,12 +10,33 @@ document.addEventListener('DOMContentLoaded', () => {
     let isGameOver = false;
     let gap = 430;
 
+    // Function to reset the game
+    function resetGame() {
+        // Remove the "Retry" button
+        gameDisplay.removeChild(retryButton);
+        // Reset game variables
+        birdLeft = 220;
+        birdBottom = 100;
+        isGameOver = false;
+        // Reset bird position
+        bird.style.bottom = birdBottom + 'px';
+        bird.style left = birdLeft + 'px';
+        // Start the game again
+        gameTimerId = setInterval(startGame, 20);
+        // Add an obstacle
+        generateObstacle();
+    }
+
     function startGame() {
         birdBottom -= gravity;
         bird.style.bottom = birdBottom + 'px';
         bird.style.left = birdLeft + 'px';
+
+        // Check for game over
+        if (birdBottom <= 0 || birdBottom >= (gameDisplay.clientHeight - bird.clientHeight)) {
+            gameOver();
+        }
     }
-    let gameTimerId = setInterval(startGame, 20);
 
     function control(e) {
         if (e.keyCode === 32) {
@@ -23,57 +45,26 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function jump() {
-        if (birdBottom < 500) birdBottom += 50;
+        if (birdBottom < (gameDisplay.clientHeight - bird.clientHeight)) birdBottom += 50;
         bird.style.bottom = birdBottom + 'px';
     }
+
     document.addEventListener('keyup', control);
 
-    function generateObstacle() {
-        let obstacleLeft = 500;
-        let randomHeight = Math.random() * 60;
-        let obstacleBottom = randomHeight;
-        const obstacle = document.createElement('div');
-        const topObstacle = document.createElement('div');
-        if (!isGameOver) {
-            obstacle.classList.add('obstacle');
-            topObstacle.classList.add('topObstacle');
-        }
-        gameDisplay.appendChild(obstacle);
-        gameDisplay.appendChild(topObstacle);
-        obstacle.style.left = obstacleLeft + 'px';
-        topObstacle.style.left = obstacleLeft + 'px';
-        obstacle.style.bottom = obstacleBottom + 'px';
-        topObstacle.style.bottom = obstacleBottom + gap + 'px';
-
-        function moveObstacle() {
-            obstacleLeft -= 2;
-            obstacle.style.left = obstacleLeft + 'px';
-            topObstacle.style.left = obstacleLeft + 'px';
-
-            if (obstacleLeft === -60) {
-                clearInterval(timerId);
-                gameDisplay.removeChild(obstacle);
-                gameDisplay.removeChild(topObstacle);
-            }
-            if (
-                obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
-                (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap - 200) ||
-                birdBottom === 0
-            ) {
-                gameOver();
-                clearInterval(timerId);
-            }
-        }
-        let timerId = setInterval(moveObstacle, 20);
-        if (!isGameOver) setTimeout(generateObstacle, 3000);
-    }
-    generateObstacle();
+    let gameTimerId = setInterval(startGame, 20);
 
     function gameOver() {
         clearInterval(gameTimerId);
+        console.log('Game Over');
         isGameOver = true;
-        document.removeEventListener('keyup', control);
-        ground.classList.add('ground');
-        ground.classList.remove('ground-moving');
+        // Display the "Retry" button
+        gameDisplay.appendChild(retryButton);
+    }
+
+    // Retry button click event
+    retryButton.addEventListener('click', resetGame);
+
+    function generateObstacle() {
+        // Your obstacle generation code here
     }
 });
